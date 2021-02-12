d322440b2601eedb882f8cc34c098071741ebc29
aaronstaes <aaron.staes@gmail.com>
2021-02-12 08:35:29 +0100

    Final Version of React Todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
+// Import components
+import Form from "./Components/Form";
+import TodoList from "./Components/TodoList"
+
 function App() {
+  // State stuff
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+    // Use effect
+    useEffect(() => {
+      filterHandler();
+    }, [todos, status]);
+
+  // Functions
+  const filterHandler = () => {
+    switch(status){
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Aaron's ToDo List</h1>
       </header>
+      <Form 
+        todos={todos} 
+        setTodos={setTodos} 
+        inputText={inputText} 
+        setInputText={setInputText} 
+        setStatus={setStatus}
+        
+      />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
     </div>
   );
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+const Form = ({inputText, setInputText, todos, setTodos, setStatus}) => {
+    // Write javacript code here
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        // Create object and update state
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
+        ]);
+        // After push, reset state
+        setInputText("");
+    };
+
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    };
+
+    return(
+        <form>
+            <input 
+                onChange={inputTextHandler} 
+                value={inputText} 
+                type="text" 
+                className="todo-input" 
+            />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
+}
+
+export default Form;
